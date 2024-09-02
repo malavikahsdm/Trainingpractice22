74432ce6e06d71f3be899f2a87dceeb17d87c1a0
malavikahsdm <user13@trainux01.jrf11vejvqfuhbwnm0ewggipoa.ax.internal.cloudapp.net>
2024-09-02 11:57:37 +0000

    Updated

---
+++ name.c
@@ -0,0 +1,10 @@
+/* this is hello world program
+with my name and address*/
+#include <stdio.h>
+int main(){
+ printf("hello world!\n");
+ printf("my name is MALAVIKA MALAV\n");
+ printf("my address is Baran Rajasthan\n");
+ return 0;
+ }
+
